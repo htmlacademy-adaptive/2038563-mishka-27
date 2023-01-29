15178b9b022f080211a19ecc4d2e56415ac2f443
Elena <len-bo@mail.ru>
2023-01-29 20:00:35 +0300

    ghjghjhgj

--- source/js/script.js
+++ source/js/script.js
@@ -2,11 +2,28 @@ const orderButton = document.querySelector('.card-product__button');
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.form-sizes');
 
-orderButton.addEventListener('click', function(){
-  modal.classList.add('modal--show');
-});
+const header = document.querySelector('.header');
+const buttonMenu = document.querySelector('.header__toggle');
+const mainNavContainer = document.querySelector('.main-nav__container');
 
-form.addEventListener('submit', function(evt){
-  evt.preventDefault();
-  modal.classList.remove('modal--show');
+if (form) {
+  orderButton.addEventListener('click', function () {
+    modal.classList.add('modal--show');
+  });
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    modal.classList.remove('modal--show');
+  });
+}
+
+header.classList.remove('header--no-js');
+header.classList.add('header--closed');
+mainNavContainer.classList.add('main-nav__container--closed');
+
+buttonMenu.addEventListener('click', function() {
+  header.classList.toggle('header--closed');
+  header.classList.toggle('header--opened');
+  mainNavContainer.classList.toggle('main-nav__container--closed');
+  mainNavContainer.classList.toggle('main-nav__container--opened');
 });
